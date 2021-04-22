c405564264f7546a2ae775919dcac9151d9dfa8b
Aiz <dev@aiz.moe>
2021-04-22 10:15:59 +0200

    use inputactions

--- src/Assets/Scripts/Menu/TouchToStart.cs
+++ src/Assets/Scripts/Menu/TouchToStart.cs
@@ -1,19 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class TouchToStart : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.touchCount > 0)
-        {
-            SceneManager.LoadScene(1);
-
-        }
+    void OnTouch(){
+        SceneManager.LoadScene(1);
     }
 }
